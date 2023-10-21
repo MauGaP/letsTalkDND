d91b74f35a640c655188f1fd9670bf04a181e404
maugap <mauro.pedano@gmail.com>
2023-10-21 14:23:59 -0400

    added a function to format the messages

--- bot.js
+++ bot.js
@@ -1,21 +1,20 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
-const { handle } = require('./commands/handleCommands');
-
+const Discord = require("discord.js");
+const config = require("./config.json");
+const { handle } = require("./commands/handleCommands");
 const { Client, GatewayIntentBits } = Discord;
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-client.once('ready', () => {
-    console.log('Bot is online!');
+client.once("ready", () => {
+  console.log("Bot is online!");
 });
 
-client.on('interactionCreate', async interaction => {
-    if (interaction.isCommand()) {
-        await handle(interaction);
-    }
+client.on("interactionCreate", async (interaction) => {
+  if (interaction.isCommand()) {
+    await handle(interaction);
+  }
 });
 
 client.login(config.DISCORD_BOT_TOKEN);
--- commands/handleCommands.js
+++ commands/handleCommands.js
@@ -1,19 +1,31 @@
-const { getAnswerFromChatGPT } = require('../utils/chatGPT');
+const { getAnswerFromChatGPT } = require("../utils/chatGPT");
+const formatResponse = require("../utils/formatResponse");
 
 const handle = async (interaction) => {
-    try {
-        // Immediately acknowledge the interaction
-        await interaction.deferReply();
+  try {
+    // Immediately acknowledge the interaction
+    await interaction.deferReply();
 
-        const userQuestion = interaction.options.getString('question');
-        const response = await getAnswerFromChatGPT(userQuestion);
+    const userQuestion = interaction.options.getString("question");
+    const userName = interaction.user.username; // Get the username from the interaction
 
-        // Then send the actual reply
-        await interaction.editReply(response);
-    } catch (error) {
-        console.error("Error:", error);
-        await interaction.followUp({ content: 'An error occurred while processing your request.' });
-    }
+    const chatGPTResponse = await getAnswerFromChatGPT(userQuestion);
+
+    // Format the response
+    const formattedAnswer = formatResponse(
+      userName,
+      userQuestion,
+      chatGPTResponse
+    );
+
+    // Then send the actual reply
+    await interaction.editReply(formattedAnswer);
+  } catch (error) {
+    console.error("Error:", error);
+    await interaction.followUp({
+      content: "An error occurred while processing your request.",
+    });
+  }
 };
 
 module.exports = { handle };
